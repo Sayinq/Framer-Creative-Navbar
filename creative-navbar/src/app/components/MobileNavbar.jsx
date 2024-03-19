5c34662fdbd353d1cdca85b10f56aea4411df670
MylesGit <37464485+Sayinq@users.noreply.github.com>
2024-03-19 16:06:43 -0400

    Works except for dropdown function not closing on menu close

--- creative-navbar/src/app/components/MobileNavbar.jsx
+++ creative-navbar/src/app/components/MobileNavbar.jsx
@@ -1,13 +1,15 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import { gsap } from 'gsap';
 import { motion, AnimatePresence } from 'framer-motion'
 import { Spin as Hamburger } from 'hamburger-react'
 import Link from 'next/link'
 import Image from 'next/image'
 
 const MobileNavbar = () => {
-    const [isOpen, setOpen] = useState(false)
-
+    const [isOpen, setOpen] = useState(false);
+    const [isDropdown, setDropdown] = useState(false);
+    
     return (
         <header className="absolute top-0 left-0 lg:hidden flex w-screen h-[60px] bg-[#F0EFEF] border-b-[1.5px] border-[#d2d2d2] p-2">
             <nav className="flex items-center w-full h-full px-2">
@@ -50,7 +52,7 @@ const MobileNavbar = () => {
                         className="absolute top-0 left-0 w-screen bg-black text-[#F0EFEF] overflow-hidden"
                     >
                         <div className="flex flex-col w-full h-full px-4 overflow-hidden">
-                            <div className="w-fit h-fit mt-[20vh] text-4xl tracking-[-0.085rem] font-black uppercase">
+                            <div className="w-fit h-fit mt-[20vh] tracking-[-0.085rem] font-black uppercase">
                                 <ul className="flex flex-col w-full h-auto gap-y-4">
                                     <Link href="">
                                         <li className="w-fit h-fit overflow-hidden">
@@ -59,6 +61,7 @@ const MobileNavbar = () => {
                                                 animate={{ translateY: isOpen ? 0 : 40 }}
                                                 exit={{ opacity: 0, transition: { delay: 0, duration: 0.3 } }}
                                                 transition={{ duration: 0.3, delay: isOpen ? 0.3 : 0, ease: 'easeInOut' }}
+                                                className="text-4xl"
                                             >
                                                 Portfolio
                                             </motion.h1>
@@ -71,6 +74,7 @@ const MobileNavbar = () => {
                                                 animate={{ translateY: isOpen ? 0 : 40 }}
                                                 exit={{ opacity: 0, transition: { delay: 0.15, duration: 0.3 } }}
                                                 transition={{ duration: 0.3, delay: isOpen ? 0.45 : 0, ease: 'easeInOut' }}
+                                                className="text-4xl"
                                             >
                                                 Pricing
                                             </motion.h1>
@@ -78,26 +82,61 @@ const MobileNavbar = () => {
                                     </Link>
                                     
                                     {/* Dropdown Services */}
-                                    <div href="" className="overflow-hidden">
+                                    <div href="" className="w-full h-fit overflow-hidden">
                                         <motion.li 
                                             initial={{ translateY: 40 }}
                                             animate={{ translateY: isOpen ? 0 : 40 }}
                                             exit={{ opacity: 0, transition: { delay: 0.3, duration: 0.3 } }}
                                             transition={{ duration: 0.3, delay: isOpen ? 0.6 : 0, ease: 'easeInOut' }}
-                                            className="flex flex-row w-fit h-fit items-center gap-x-2 overflow-hidden"
+                                            className="flex flex-row w-fit h-fit items-center gap-x-2 overflow-hidden text-4xl cursor-pointer"
+                                            onClick={() => setDropdown(!isDropdown)} 
                                         >
-                                            <div clasName="w-fit h-fit">
+                                            <div className="w-fit h-fit">
                                                 <h1>Services</h1>
                                             </div>
-                                            <div className="w-fit h-fit sm:-translate-y-0 -translate-y-[4px]">
+                                            <motion.div 
+                                                initial={{ rotate: 0 }}
+                                                animate={{ rotate: isDropdown ? 0 : "180deg" }}
+                                                exit={{ rotate: 0, transition: { delay: 0.3, duration: 0.3 }}}
+                                                transition={{ duration: 0.3, ease: 'easeInOut' }}
+                                                className="w-fit h-fit translate-y-[40px]"
+                                            >
                                                 <Image
                                                     src="/plus.svg"
                                                     alt="Add Icon"
                                                     width={35}
                                                     height={35}
                                                 />
-                                            </div>
+                                            </motion.div>
                                         </motion.li>
+                                        <AnimatePresence>
+                                            {isDropdown && (
+                                                <motion.div
+                                                    initial={{ height: 0 }}
+                                                    animate={{ height: isDropdown ? 170 : 0 }}
+                                                    exit={{ height: 0 }}
+                                                    transition={{ duration: 0.3, ease: 'easeInOut' }}
+                                                    className="w-full h-fit overflow-hidden"
+                                                >
+                                                    <div className="flex flex-col w-full h-fit gap-y-4 mt-2">
+                                                        {/* Development */}
+                                                        <div className="flex justify-center items-center w-full h-[40px] rounded-full hover:border-0 border-[0.5px] border-white/30 hover:bg-[#F0EFEF] hover:text-black hover:tracking-normal transition-all duration-300 ease-in-out uppercase gap-x-2 cursor-pointer overflow-hidden">
+                                                            <h1>Development</h1>
+                                                        </div>
+
+                                                        {/* Optimization */}
+                                                        <div className="flex justify-center items-center w-full h-[40px] rounded-full hover:border-0 border-[0.5px] border-white/30 hover:bg-[#F0EFEF] hover:text-black hover:tracking-normal transition-all duration-300 ease-in-out uppercase gap-x-2 cursor-pointer overflow-hidden">
+                                                            <h1>Optimization</h1>
+                                                        </div>
+
+                                                        {/* Custom Design */}
+                                                        <div className="flex justify-center items-center w-full h-[40px] rounded-full hover:border-0 border-[0.5px] border-white/30 hover:bg-[#F0EFEF] hover:text-black hover:tracking-normal transition-all duration-300 ease-in-out uppercase gap-x-2 cursor-pointer overflow-hidden">
+                                                            <h1>Custom Design</h1>
+                                                        </div>
+                                                    </div>
+                                                </motion.div>
+                                            )}
+                                        </AnimatePresence>
                                     </div>
                                     <Link href="">
                                         <li className="w-fit h-fit overflow-hidden">
@@ -106,6 +145,7 @@ const MobileNavbar = () => {
                                                 animate={{ translateY: isOpen ? 0 : 40 }}
                                                 exit={{ opacity: 0, transition: { delay: 0.45, duration: 0.3 } }}
                                                 transition={{ duration: 0.3, delay: isOpen ? 0.75 : 0, ease: 'easeInOut' }}
+                                                className="text-4xl"
                                             >
                                                 Blog
                                             </motion.h1>
@@ -118,8 +158,8 @@ const MobileNavbar = () => {
                                 animate={{ opacity: isOpen ? 1 : 0 }}
                                 exit={{ opacity: 0, transition: {delay: 0.7, duration: 0.3 }}}
                                 transition={{ duration: 0.3, delay: isOpen ? 1 : 0, ease: 'easeInOut' }}
-                                className="w-fit h-auto overflow-hidden">
-                                <div className="flex justify-center items-center w-fit h-[40px] rounded-full border-[0.5px] border-white/30 uppercase gap-x-2 mt-[2rem] cursor-pointer overflow-hidden">
+                                className="max-w-[220px] w-full h-auto overflow-hidden">
+                                <div className="flex justify-center items-center w-full h-[40px] rounded-full border-[0.5px] border-white/30 uppercase gap-x-2 mt-[2rem] cursor-pointer overflow-hidden">
                                     <div className="flex flex-row justify-center items-center w-fit h-fit px-4 gap-x-2">
                                         <div className="w-fit h-fit tracking-[-0.085rem]">
                                             <h1>Get in touch</h1>
@@ -141,7 +181,7 @@ const MobileNavbar = () => {
                                 animate={{ translateX: isOpen ? 0 : -250, opacity: isOpen ? 1 : 0 }}
                                 exit={{ translateX : -250, opacity: 0, transition: { delay: 0.9 , duration: 0.5 }}}
                                 transition={{ duration: 0.5, delay: isOpen ? 1.15 : 0, ease: 'easeInOut' }}
-                                className="flex flex-col gap-y-4 mt-[25vh] overflow-hidden"
+                                className="fixed bottom-[20px] left-[20px] flex flex-col gap-y-4 overflow-hidden"
                             >
                                 <div className="w-fit h-fit text-md tracking-[-0.085rem] uppercase">
                                     <h1>emailus@email.com</h1>
